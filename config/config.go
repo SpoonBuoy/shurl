91ab3ce9168d54b8c8dd0774514cd10312b1d31e
Arsalan Rashid <arsalanrashidthoker@gmail.com>
2023-11-30 16:51:48 +0530

    add config

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Port          string `json:"port"`
+	Host          string `json:"host"`
+	RedisAddr     string `json:"redis-addr"`
+	RedisPassword string `json:"redis-password"`
+	RedisDB       int    `json:"redis-db"`
+}
+
+var ShurlConfig Config
+
+func init() {
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatalf("Error loading config %v", err)
+	}
+	ShurlConfig = config
+}
+
+func loadConfig(filePath string) (Config, error) {
+	var config Config
+	//filePath = filepath.Join("..", filePath)
+	file, err := os.Open(filePath)
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
+func GetConfig() Config {
+
+	return ShurlConfig
+}
